2fefae4915191890e78bc76091bf57693ea76768
Liliia Erygina <60519890+pilulek@users.noreply.github.com>
2025-01-26 20:31:05 +0200

    Hw01 gradle (#1)
    * hw01-gradle
    
    * hw01-gradle

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
+
+plugins {
+    idea
+    id("io.spring.dependency-management")
+    id("org.springframework.boot") apply false
+}
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(21)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+allprojects {
+    group = "ru.otus"
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+    val guava: String by project
+
+    apply(plugin = "io.spring.dependency-management")
+    dependencyManagement {
+        dependencies {
+            imports {
+                mavenBom(BOM_COORDINATES)
+            }
+            dependency("com.google.guava:guava:$guava")
+        }
+    }
+
+    plugins.apply(JavaPlugin::class.java)
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "otus-java-erygina-2024-12"
+include("hw01-gradle")
+
+pluginManagement {
+    val dependencyManagement: String by settings
+    val springframeworkBoot: String by settings
+    val johnrengelmanShadow: String by settings
+
+    plugins {
+        id("io.spring.dependency-management") version dependencyManagement
+        id("org.springframework.boot") version springframeworkBoot
+        id("com.github.johnrengelman.shadow") version johnrengelmanShadow
+    }
+}
